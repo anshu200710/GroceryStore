5d48f14c01340ad47fe40b171b929c5f58a21da0
Vishwash-chauhan <vishwashchauhan6231@gmail.com>
2026-01-21 12:55:15 +0530

    replaced logo commented out online payment option

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -290,7 +290,7 @@ const Cart = () => {
                         className="w-full border border-gray-300 bg-white px-3 py-2 mt-2 outline-none"
                     >
                         <option value="COD">Cash On Delivery</option>
-                        <option value="Online">Online Payment</option>
+                        {/* <option value="Online">Online Payment</option> */}
                     </select>
                 </div>
 
