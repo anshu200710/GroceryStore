235f99e7e85e7488e0e75cfe433ed00bf4f625c8
Vishwash-chauhan <vishwashchauhan6231@gmail.com>
2026-01-28 14:48:17 +0530

    updaed cloudinary

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -2,14 +2,16 @@ import asyncHandler from "express-async-handler";
 import { v2 as cloudinary } from "cloudinary";
 import CustomError from "../utils/CustomError.js";
 
+import { CLOUDINARY_NAME } from "../config/index.js";
+
 export const getUploadSignature = asyncHandler(async (req, res) => {
-  // requires CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME in env
+  // requires CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_NAME in env
   const timestamp = Math.floor(Date.now() / 1000);
   const signature = cloudinary.utils.api_sign_request({ timestamp }, process.env.CLOUDINARY_API_SECRET);
   res.json({
     signature,
     timestamp,
     apiKey: process.env.CLOUDINARY_API_KEY,
-    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudName: CLOUDINARY_NAME || process.env.CLOUDINARY_NAME,
   });
 });
